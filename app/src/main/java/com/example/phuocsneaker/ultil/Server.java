f76001a2c5713799282233f4ea21573f6047d9a3
phuoc <hongphuoc0330@gmail.com>
2019-07-30 22:02:48 +0700

    hello word

---
+++ app/src/main/java/com/example/phuocsneaker/ultil/Server.java
@@ -0,0 +1,12 @@
+package com.example.phuocsneaker.ultil;
+
+public class Server {
+    public static String localhost = "172.20.10.5";
+    public static String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
+    public static String Duongdansanphammoinhat = "http://" + localhost + "/server/getsanphammoinhat.php";
+    public static String Duongdanadidas = "http://" + localhost + "/server/getsanpham.php?page=";
+    public static String Duongdannike = "http://" + localhost + "/server/getsanphamnike.php?page=";
+    public static String Duongdanfila = "http://" + localhost + "/server/getsanphamfila.php?page=";
+    public static String Duongdandonhang = "http://" + localhost + "/server/thongtinkhachhang.php";
+    public static String Duongdanchitietdonhang = "http://" + localhost + "/server/chitietdonhang.php";
+}
